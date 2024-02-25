24491c2dad713b4998ecac6bc8bf62f8a3f20cae
George <george.vadakumchery96@gmail.com>
2024-02-25 22:57:12 +0530

    feat: 	--chat push!--

---
+++ js/index.js
@@ -0,0 +1,45 @@
+
+
+
+let steem = () => {
+    fetch('http://127.0.0.1:3001/art_work') // Adjust the URL to match your backend endpoint
+        .then(response => response.json())
+        .then(data => {
+            // Process the fetched JSON data
+            displayObjectFields(data)
+        })
+        .catch(error => {
+            displayErrorMessage(error);
+            console.log('Error fetching data:', error);
+        });
+}
+// Function to display object fields in HTML
+
+function displayErrorMessage(message) {
+    // Get the error message div
+    const errorMessageDiv = document.getElementById('errorMessage');
+
+    // Set the error message in the div
+    errorMessageDiv.textContent = message;
+}
+
+function displayObjectFields(data) {
+
+
+    let objectList = document.getElementById('objectList');
+
+    // Iterate through the array of objects
+    data.forEach(function (obj) {
+        // Create a new <li> element
+        let listItem = document.createElement('li');
+
+        // Set the inner text of the <li> element to the value of the 'name' field
+        listItem.textContent = obj.title;
+
+        // Append the <li> element to the <ul> element
+        objectList.appendChild(listItem);
+    });
+
+
+}
+
